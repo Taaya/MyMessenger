b25efff054de1185291a1775910ff6101cfb8366
Taya_ <taya.buzukina@gmail.com>
2017-05-24 13:34:08 +0300

    added simplified paths in routes.rb, modified Contact page, static_page_spec.rb modified.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,42 +4,44 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'My Messenger'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('My messenger')
+    it "should have the h1 'My Messenger'" do
+      visit '/'
+      expect(page).to have_content('My Messenger')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit '/'
       expect(page).to have_title("My Messenger")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit '/'
       expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
+    it "should have the h1 'Help'" do
+      visit 'help'
       expect(page).to have_content('Help')
     end
+
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit 'help'
       expect(page).to have_title("My Messenger | Help")
     end
   end
 
   describe "About page" do
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+    it "should have the h1 'About Us'" do
+      visit 'about'
       expect(page).to have_content('About Us')
     end
+
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit 'about'
       expect(page).to have_title("My Messenger | About Us")
     end
   end
@@ -47,12 +49,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit 'contact'
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit 'contact'
       expect(page).to have_title("My Messenger | Contact")
     end
   end
