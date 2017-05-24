c867c146dd8ff3925f9cec1023b9431e67857693
Taya_ <taya.buzukina@gmail.com>
2017-05-24 12:56:24 +0300

    added Contact page: html, controller, action, routes.rb changed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,17 @@ describe "Static pages" do
       expect(page).to have_title("My Messenger | About Us")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("My Messenger | Contact")
+    end
+  end
 end
