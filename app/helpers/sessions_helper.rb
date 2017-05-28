2ae992907d97696a9117ab11ff2d785a0e273879
Taya_ <taya.buzukina@gmail.com>
2017-05-28 14:14:06 +0300

    moved microposts to home page, added time created added deleting of microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -42,4 +42,11 @@ module SessionsHelper
   def admin?
     user[:admin]
   end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
 end
